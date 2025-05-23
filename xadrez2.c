70cdf065d0f9938edd35157a9850fd5c8e42893a
Margaret Santos de Oliveira <margaretsantos.adv@outlook.com>
2025-05-23 17:14:06 +0000

    Entrada de dados 2

--- xadrez2.c
+++ xadrez2.c
@@ -38,4 +38,26 @@ int main() {
     printf("Informe o número de pontos turísticos: ");
     scanf("%d", &carta1.pontos_turisticos);
 
+    printf("\nCadastro da carta 2:\n");
+    printf("Informe o estado (sigla, ex: RJ): ");
+    scanf(" %s", carta2.estado);
+
+    printf("Informe o código da carta (ex: B02): ");
+    scanf(" %s", carta2.codigo);
+
+    printf("Informe o nome da cidade: ");
+    scanf(" %[^\n]", carta2.nome_cidade);
+
+    printf("Informe a população: ");
+    scanf("%d", &carta2.populacao);
+
+    printf("Informe a área (em km²): ");
+    scanf("%f", &carta2.area);
+
+    printf("Informe o PIB (em bilhões): ");
+    scanf("%f", &carta2.pib);
+
+    printf("Informe o número de pontos turísticos: ");
+    scanf("%d", &carta2.pontos_turisticos);
+
 }
