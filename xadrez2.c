413c01fb06a13beca8a31e493a5ed6981c8df9c9
Margaret Santos de Oliveira <margaretsantos.adv@outlook.com>
2025-05-23 17:13:11 +0000

    Entrada de dados 1 e declaração inicial

--- xadrez2.c
+++ xadrez2.c
@@ -10,3 +10,32 @@ struct Carta {
     float pib;                   
     int pontos_turisticos;
 };
+
+int main() {
+    struct Carta carta1, carta2;
+    float densidade1, densidade2;
+    float pibpercapita1, pibpercapita2;
+
+    printf("Cadastro da carta 1:\n");
+    printf("Informe o estado (sigla, ex: SP): ");
+    scanf(" %s", carta1.estado);
+
+    printf("Informe o código da carta (ex: A01): ");
+    scanf(" %s", carta1.codigo);
+
+    printf("Informe o nome da cidade: ");
+    scanf(" %[^\n]", carta1.nome_cidade);
+
+    printf("Informe a população: ");
+    scanf("%d", &carta1.populacao);
+
+    printf("Informe a área (em km²): ");
+    scanf("%f", &carta1.area);
+
+    printf("Informe o PIB (em bilhões): ");
+    scanf("%f", &carta1.pib);
+
+    printf("Informe o número de pontos turísticos: ");
+    scanf("%d", &carta1.pontos_turisticos);
+
+}
