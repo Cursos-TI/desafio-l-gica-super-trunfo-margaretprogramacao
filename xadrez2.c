96f84aa5e088f7c5385eb6cddb46f7a084bb5ca0
Margaret Santos de Oliveira <margaretsantos.adv@outlook.com>
2025-05-23 17:11:55 +0000

    Estrutura inicial para apresentar as cartas

---
+++ xadrez2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+struct Carta {
+    char codigo[4];
+    char estado[3];               
+    char nome_cidade[100];
+    int populacao;
+    float area;
+    float pib;                   
+    int pontos_turisticos;
+};
